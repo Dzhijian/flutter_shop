56ed925b733217d0f01c22f5a7e87393f6b798da
james <646724452@qq.com>
2019-09-07 12:55:39 +0800

    添加Tabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text('首页'),
       ),
     );
   }
+
+  // 异步请求
+  void getHttp() async {
+    try {
+      Response response;
+      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=长腿小姐姐');
+      return print(response);
+    } catch (e) {
+      return print(e);
+    }
+  }
+
+
 }
