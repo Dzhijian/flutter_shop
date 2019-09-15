6a22768f8a035da789850a00ad152a56a09c87c9
dengzj <dengzj@canplay.com.cn>
2019-09-16 00:51:25 +0800

    添加首页轮播图，添加接口数据

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-class HomePage extends StatelessWidget {
+class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据';
+  // @override
+  //   void initState() {
+  //     // 获取首页数据
+  //     getHomePageContent().then((val){
+  //       setState(() {
+  //         homePageContent = val.toString();
+  //         // print(homePageContent);
+  //       });
+  //     });
+  //     super.initState();
+  //   }
   @override
   Widget build(BuildContext context) {
-    getHttp();
     return Scaffold(
-      body: Center(
-        child: Text('首页'),
+      appBar: AppBar(title: Text('百姓生活+')),
+      body:FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context,snapshort){
+          print('snapshort------> ${snapshort.data}');
+          if(snapshort.hasData){
+            var data = json.decode(snapshort.data.toString());
+            List<Map> swiperList = (data['data']['slides'] as List).cast(); 
+            print("swiperList ===> ${swiperList}");
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDataList: swiperList)
+              ],
+            );
+          }else{
+            return Center(
+              child: Text('没有数据'),
+            );
+          }
+        }
+      )
+    );
+  }
+}
+
+// 首页轮播图组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  SwiperDiy({this.swiperDataList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index){
+          print('image-->${swiperDataList[index]['image']}');
+          return Image.network("${swiperDataList[index]['image']}",fit: BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        // page 页码
+        pagination: SwiperPagination(),
+        // 自动播放
+        autoplay: true,
       ),
     );
   }
-
-  // 异步请求
-  void getHttp() async {
-    try {
-      Response response;
-      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=长腿小姐姐');
-      return print(response);
-    } catch (e) {
-      return print(e);
-    }
-  }
-
-
 }
